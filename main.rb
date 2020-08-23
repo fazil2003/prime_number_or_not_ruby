273394d7918ee5cbc080f6c4c9e014e0e06a8e6a
Mohamed Fazil <65252590+fazil2003@users.noreply.github.com>
2020-08-23 14:58:14 +0530

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,16 @@
+# Ruby program for prime? function 
+   
+# Pre-defined class 
+require 'prime'
+   
+# Initializing the numbers  
+num1 = 100
+num2 = 17
+num3 = 90
+num4 = 29
+      
+# Printing if prime or not  
+puts num1.prime? 
+puts num2.prime? 
+puts num3.prime? 
+puts num4.prime? 
